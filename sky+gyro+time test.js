5bca32c6c75671f08c211ebfe5907234f8126a8a
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2023-05-04 11:31:45 -0400

    a

--- sky+gyro+time test.js
+++ sky+gyro+time test.js
@@ -19,18 +19,108 @@
 			init();
 			rock();
 			render();
+			clouds(); 
 			fetch(url)
 			.then(response => response.json())
 			.then((data) => { console.log(data)
 				const weather = data.weather[0].main;
 				if(weather === 'Rainy'|| weather === 'Clouds'){
-					rainy(); 
+					
 					initSky();
 				} else {
 					initSky1();
 				}
 
 			})
+
+			
+			function clouds() {
+				scene.fog = new THREE.FogExp2(0x11111f, 2);
+				scene.background = new THREE.Color(0x000000); 
+
+				let cloudPartices = [];
+
+				
+
+				const starGeo = new THREE.BufferGeometry ()
+				const vertices = [];
+				for (let i = 0; i < 15000; i++) {
+				const x = Math.random() * 400 - 300;
+				const y = Math.random() * 500 - 250;
+				const z = Math.random() * 400 - 200;
+				vertices.push(x, y, z);
+				}
+				starGeo.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+				let starMaterial = new THREE.PointsMaterial({
+					color:0xaaaaaa,
+					size:0.5,
+					transparent: true
+				})
+				const stars = new THREE.Points(starGeo,starMaterial)
+				//scene.add(stars)
+				
+				
+				//Cloud
+				const loader = new THREE.TextureLoader();
+				const cloudVertices = [];
+				loader.load("./smoke.png", function(texture){
+	
+					const cloudGeo = new THREE.PlaneGeometry(2000,2000);
+					const cloudMaterial = new THREE.MeshLambertMaterial({
+					map: texture,
+					transparent: true
+					});
+	
+					for(let p=0; p<25; p++) {
+					let cloud = new THREE.Mesh(cloudGeo,cloudMaterial);
+					cloud.position.set(
+						Math.random()*800 -400,
+						500,
+						Math.random()*500 - 450
+					);
+					cloud.rotation.x = 1.16;
+					cloud.rotation.y = -0.12;
+					cloud.rotation.z = Math.random()*360;
+					cloud.material.opacity = 0.08;
+					cloudVertices.push(cloud);
+					scene.add(cloud);
+					}})
+				
+				//flash
+				const flash = new THREE.PointLight(0x062d89, 30, 500 ,1.7);
+				flash.position.set(200,300,100);
+				//scene.add(flash);
+				
+				function animate() {
+					requestAnimationFrame(animate);
+					cloudVertices.forEach(p => {
+						p.rotation.z -=0.002;
+					  });
+					const positions = starGeo.attributes.position.array;
+					for (let i = 1; i < positions.length; i += 3) {
+						if (positions[i] < -200) {
+						positions[i] = 200;
+						}
+						positions[i] -= 4;
+					}
+					starGeo.attributes.position.needsUpdate = true;
+					stars.rotation.y +=0.002;
+					
+					renderer.render(scene, camera); 
+					if(Math.random() > 0.93 || flash.power > 100) {
+						if(flash.power < 100) 
+						  flash.position.set(
+							Math.random()*400,
+							300 + Math.random() *200,
+							100
+						  );
+						flash.power = 50 + Math.random() * 500;
+					  }
+				  }
+	
+				animate();
+
+			}
 
 			function rock(){
 			
--- sky+gyro+time+rock copy.js
+++ sky+gyro+time+rock copy.js
@@ -167,13 +167,37 @@ function rainy() {
 
 
 function clouds() {
+	scene.fog = new THREE.FogExp2(0x11111f, 2);
+	scene.background = new THREE.Color(0x000000); 
+
 	let cloudPartices = [];
 
+	
+
+	const starGeo = new THREE.BufferGeometry ()
+	const vertices = [];
+	for (let i = 0; i < 15000; i++) {
+	const x = Math.random() * 400 - 300;
+	const y = Math.random() * 500 - 250;
+	const z = Math.random() * 400 - 200;
+	vertices.push(x, y, z);
+	}
+	starGeo.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+	let starMaterial = new THREE.PointsMaterial({
+		color:0xaaaaaa,
+		size:0.5,
+		transparent: true
+	})
+	const stars = new THREE.Points(starGeo,starMaterial)
+	//scene.add(stars)
+	
+	
+	//Cloud
 	const loader = new THREE.TextureLoader();
 	const cloudVertices = [];
 	loader.load("./smoke.png", function(texture){
 
-		const cloudGeo = new THREE.PlaneGeometry(100,100);
+		const cloudGeo = new THREE.PlaneGeometry(2000,2000);
 		const cloudMaterial = new THREE.MeshLambertMaterial({
 		map: texture,
 		transparent: true
@@ -187,20 +211,47 @@ function clouds() {
 			Math.random()*500 - 450
 		);
 		cloud.rotation.x = 1.16;
-		cloud.rotation.y = -10.12;
+		cloud.rotation.y = -0.12;
 		cloud.rotation.z = Math.random()*360;
-		cloud.material.opacity = 0.10;
+		cloud.material.opacity = 0.08;
 		cloudVertices.push(cloud);
 		scene.add(cloud);
 		}})
+	
+	//flash
+	const flash = new THREE.PointLight(0x062d89, 30, 500 ,1.7);
+	flash.position.set(200,300,100);
+	//scene.add(flash);
 	
 	function animate() {
 		requestAnimationFrame(animate);
 		cloudVertices.forEach(p => {
 			p.rotation.z -=0.002;
 		  });
+		const positions = starGeo.attributes.position.array;
+		for (let i = 1; i < positions.length; i += 3) {
+			if (positions[i] < -200) {
+			positions[i] = 200;
+			}
+			positions[i] -= 4;
 		}
+		starGeo.attributes.position.needsUpdate = true;
+		stars.rotation.y +=0.002;
+		
+		renderer.render(scene, camera); 
+		if(Math.random() > 0.93 || flash.power > 100) {
+			if(flash.power < 100) 
+			  flash.position.set(
+				Math.random()*400,
+				300 + Math.random() *200,
+				100
+			  );
+			flash.power = 50 + Math.random() * 500;
+		  }
+	  }
+
 	animate();
+
 }
 
 function cloudSky() {	
